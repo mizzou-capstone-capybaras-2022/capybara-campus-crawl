bb389dc9ebaed993fc80e3ebb72562c22b45cbfc
Michael Navazhylau <mankbf@umsystem.edu>
2022-02-01 09:14:38 -0600

    Added Capybara Database seedeR

---
+++ backend/CapybaraDatabaseSeeder/src/main/java/com/capybara/CapybaraCampusCrawl/databaseseeder/CapybaraDatabaseSeeder/CapybaraDatabaseSeederApplication.java
@@ -0,0 +1,13 @@
+package com.capybara.CapybaraCampusCrawl.databaseseeder.CapybaraDatabaseSeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapybaraDatabaseSeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapybaraDatabaseSeederApplication.class, args);
+	}
+
+}
---
+++ backend/CapybaraDatabaseSeeder/src/test/java/com/capybara/CapybaraCampusCrawl/databaseseeder/CapybaraDatabaseSeeder/CapybaraDatabaseSeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capybara.CapybaraCampusCrawl.databaseseeder.CapybaraDatabaseSeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapybaraDatabaseSeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
