7adb91133ca4ca55b1320885a970e8440680cfb1
Michael Navazhylau <mankbf@umsystem.edu>
2022-03-09 17:03:44 -0600

    skeleton of ORS dao in progress

--- backend/CapybaraCampusCrawlBackend/src/main/java/com/capybara/CapybaraCampusCrawlBackend/CapybaraCampusCrawlBackendApplication.java
+++ backend/CapybaraCampusCrawlBackend/src/main/java/com/capybara/CapybaraCampusCrawlBackend/CapybaraCampusCrawlBackendApplication.java
@@ -1,13 +1,17 @@
 package com.capybara.CapybaraCampusCrawlBackend;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -42,6 +46,9 @@ public class CapybaraCampusCrawlBackendApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(CapybaraCampusCrawlBackendApplication.class);
 	
+	@Autowired
+	private Environment env;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CapybaraCampusCrawlBackendApplication.class, args);
 	}
@@ -56,5 +63,19 @@ public class CapybaraCampusCrawlBackendApplication {
 	    return new ImplicitNamingStrategyLegacyJpaImpl();
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+        	String apiKey = env.getProperty("openrouteservice.api_key");
+            String orsRunner = restTemplate.getForObject(
+                    "https://api.openrouteservice.org/v2/directions/foot-walking?api_key=" + apiKey + "&start=-92.32310414,38.94131544&end=-92.32290566,38.93858664", String.class);
+            logger.info(orsRunner);
+        };
+    }
 	
 }
