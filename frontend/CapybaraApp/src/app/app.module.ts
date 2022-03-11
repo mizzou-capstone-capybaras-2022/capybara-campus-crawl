5be90ec5db0bb4c46726d3003a8beea96555d2d8
Olivia LaVal <oj.laval@gmail.com>
2022-03-11 15:56:22 -0600

    Set up form Input Locations
    Set up a form and formgroup/formcontrol under the Input Locations tab

--- frontend/CapybaraApp/src/app/app.module.ts
+++ frontend/CapybaraApp/src/app/app.module.ts
@@ -8,18 +8,29 @@ import { MatTabsModule } from '@angular/material/tabs';
 
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputLocationsComponent } from './input-locations/input-locations.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    InputLocationsComponent
   ],
   imports: [
     BrowserModule,
     MatTabsModule,
     AppRoutingModule,
     LeafletModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
