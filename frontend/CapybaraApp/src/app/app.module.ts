7702f2b73ecce0e1c49b499e997c065a3c2b6ab2
Alex Centorbi <alexcentorbi07@gmail.com>
2022-03-22 19:49:35 -0500

    Form Updates
    partly completed the 3 form sections

--- frontend/CapybaraApp/src/app/app.module.ts
+++ frontend/CapybaraApp/src/app/app.module.ts
@@ -14,13 +14,20 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import { ComponentAddConstraintsComponent } from './component-add-constraints/component-add-constraints.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { SearchBuildingsComponent } from './search-buildings/search-buildings.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    InputLocationsComponent
+    InputLocationsComponent,
+    ComponentAddConstraintsComponent,
+    SearchBuildingsComponent
   ],
   imports: [
     BrowserModule,
@@ -31,8 +38,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatFormFieldModule,
     MatSelectModule,
     FormsModule,ReactiveFormsModule,
-    MatButtonModule
-
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/CapybaraApp/src/app/component-add-constraints/component-add-constraints.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-component-add-constraints',
+  templateUrl: './component-add-constraints.component.html',
+  styleUrls: ['./component-add-constraints.component.scss']
+})
+export class ComponentAddConstraintsComponent implements OnInit {
+
+  addConstraints = new FormGroup({
+    indoor: new FormControl(''),
+    food: new FormControl('')
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addConstraintsfn() {
+    console.log(this.addConstraints.value);
+  }
+  isShown: boolean = false ;
+  toggleShow() {
+
+    this.isShown = ! this.isShown;
+    
+    }
+}
