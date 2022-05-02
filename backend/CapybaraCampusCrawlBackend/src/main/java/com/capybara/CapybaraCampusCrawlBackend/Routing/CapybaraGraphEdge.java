e3f53381c54995ac1943565c93e99fa86791e5f9
Mechasparrow <mikipux7@gmail.com>
2022-05-02 08:41:27 -0500

    Minimized routing complexity

--- backend/CapybaraCampusCrawlBackend/src/main/java/com/capybara/CapybaraCampusCrawlBackend/Routing/CapybaraGraphEdge.java
+++ backend/CapybaraCampusCrawlBackend/src/main/java/com/capybara/CapybaraCampusCrawlBackend/Routing/CapybaraGraphEdge.java
@@ -14,4 +14,5 @@ public class CapybaraGraphEdge {
 			this.coords = coords;
 			this.distance = distance;
 		}
+
 	}
--- backend/CapybaraCampusCrawlBackend/src/main/java/com/capybara/CapybaraCampusCrawlBackend/Routing/RoutingSystem.java
+++ backend/CapybaraCampusCrawlBackend/src/main/java/com/capybara/CapybaraCampusCrawlBackend/Routing/RoutingSystem.java
@@ -1,6 +1,7 @@
 package com.capybara.CapybaraCampusCrawlBackend.Routing;
 
-import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.graph.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,18 +25,14 @@ import javax.inject.Inject;
 
 import org.jgrapht.*;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.DefaultUndirectedGraph;
-import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component
 public class RoutingSystem {
-
 	private static final Logger logger = LoggerFactory.getLogger(CapybaraCampusCrawlBackendApplication.class);
 
-	SimpleDirectedGraph<GraphNode, CapybaraGraphEdge> capybaraGraph;
+	SimpleDirectedWeightedGraph<Long, CapybaraGraphEdge> capybaraGraph;
 
 	@Inject
 	public RoutingSystem(GraphEdgeRepository edgeDao, GraphNodeRepository nodeDao) {
@@ -46,65 +43,53 @@ public class RoutingSystem {
 
 		try {
 			capybaraGraph = constructGraph(nodes, edges);
+			logger.info("Graph constructed");
 		} catch (JsonProcessingException e) {
-			capybaraGraph = new SimpleDirectedGraph<>(CapybaraGraphEdge.class);
+			capybaraGraph = new SimpleDirectedWeightedGraph<>(CapybaraGraphEdge.class);
 		}
 	}
 
-	public SimpleDirectedGraph<GraphNode, CapybaraGraphEdge> constructGraph(List<GraphNode> nodes, List<GraphEdge> edges) throws JsonProcessingException {
-		SimpleDirectedGraph<GraphNode, CapybaraGraphEdge> capybaraGraph = new SimpleDirectedGraph<>(CapybaraGraphEdge.class);
+	public List<Point> ComputeRoute(Long startingNodeId, Long endingNodeId) {
+		DijkstraShortestPath<Long, CapybaraGraphEdge> dijkstraAlg = new DijkstraShortestPath<>(capybaraGraph);
+		ShortestPathAlgorithm.SingleSourcePaths<Long, CapybaraGraphEdge> pathsFromStart = dijkstraAlg.getPaths(startingNodeId);
+		GraphPath<Long, CapybaraGraphEdge> shortestPath = pathsFromStart.getPath(endingNodeId);
 
+		List<Point> routePoints = new ArrayList<>();
+		for (CapybaraGraphEdge edge: shortestPath.getEdgeList()){
+			routePoints.addAll(edge.coords);
+		}
 
-
-		return capybaraGraph;
-	}
-
-	/*
-
-	public List<Point> ComputeRoute(Long startingBuildingId, Long endingBuildingId) {
-		List<Point> routePoints = GetRouteBetweenPoints(startingBuildingId, endingBuildingId, nodeList);
+		logger.info(shortestPath.toString());
 		return routePoints;
 	}
-	private static List<CapybaraRouteNode> generateNodes(List<GraphNode> graphNodeList, List<GraphEdge> graphEdgeList) throws JsonProcessingException{
-		List<CapybaraRouteNode> capybaraNodeList = constructNodes(graphNodeList);
-		capybaraNodeList = addCapybaraEdges(capybaraNodeList, graphEdgeList);
-	    return capybaraNodeList; 
-	}
-	private static List<CapybaraRouteNode> constructNodes(List<GraphNode> graphNodeList){
-		List<CapybaraRouteNode> capybaraNodeList = new ArrayList<CapybaraRouteNode>();
-		
-		//generate nodes
-		for (GraphNode currentNode : graphNodeList) {
-			int nodeID = (int) (currentNode.getNodeID() - 1);
-	    	String nodeDesc = currentNode.getDescription();
-	    	double lat = currentNode.getLatitude();
-	    	double lon = currentNode.getLongitude();
-	    	
-	    	CapybaraRouteNode capybaraNode = new CapybaraRouteNode(nodeID,nodeDesc,lat,lon);
-	    	capybaraNodeList.add(capybaraNode);
+
+	public SimpleDirectedWeightedGraph<Long, CapybaraGraphEdge> constructGraph(List<GraphNode> nodes, List<GraphEdge> edges) throws JsonProcessingException {
+		SimpleDirectedWeightedGraph<Long, CapybaraGraphEdge> capybaraGraph = new SimpleDirectedWeightedGraph<>(CapybaraGraphEdge.class);
+
+		//Add the vertex of the graph
+		for (GraphNode node: nodes){
+			Long nodeId = node.getNodeID();
+			capybaraGraph.addVertex(nodeId);
 		}
-		
-		return capybaraNodeList;
-	}
-	private static List<CapybaraRouteNode> addCapybaraEdges(List<CapybaraRouteNode> capybaraNodeList, List<GraphEdge> graphEdgeList) throws JsonProcessingException{
-		//generate adj lists for every node
-	    for (GraphEdge currentEdge: graphEdgeList) {
-	    	int fromNode = Math.toIntExact(currentEdge.getFromNode().getNodeID());
-		    int toNode = Math.toIntExact(currentEdge.getToNode().getNodeID());
-		    
-		    //get distance and modify for outside
-		    double distance = getModifiedGraphEdgeWeight(currentEdge);
 
-			List<Point> points = parseJSONPoints(currentEdge.getPathshape());
+		for (GraphEdge edge : edges){
+			List<Point> edgeCoords = parseJSONPoints(edge.getPathshape());
+			List<Point> reverseCoords = new ArrayList<>(edgeCoords);
 
-			List<Point> reversedPoints = new ArrayList<>(points);
-			Collections.reverse(reversedPoints);
+			Collections.reverse(reverseCoords);
 
-			capybaraNodeList.get(fromNode-1).addEdge(capybaraNodeList.get(toNode-1), distance,points);
-			capybaraNodeList.get(toNode-1).addEdge(capybaraNodeList.get(fromNode-1), distance,reversedPoints);
-	    }
-	    
-	    return capybaraNodeList;
+			double distance = getModifiedGraphEdgeWeight(edge);
+			CapybaraGraphEdge capybaraEdge = new CapybaraGraphEdge(edgeCoords, distance);
+			CapybaraGraphEdge reverseCapybaraEdge = new CapybaraGraphEdge(reverseCoords, distance);
+
+			capybaraGraph.addEdge(edge.getFromNode().getNodeID(), edge.getToNode().getNodeID(), capybaraEdge);
+			capybaraGraph.addEdge(edge.getToNode().getNodeID(), edge.getFromNode().getNodeID(), reverseCapybaraEdge);
+
+			capybaraGraph.setEdgeWeight(capybaraEdge, capybaraEdge.distance);
+			capybaraGraph.setEdgeWeight(reverseCapybaraEdge, reverseCapybaraEdge.distance);
+		}
+
+		return capybaraGraph;
 	}
 
 	private static double getModifiedGraphEdgeWeight(GraphEdge currentEdge){
@@ -131,83 +116,7 @@ public class RoutingSystem {
 
 		return points;
 	}
-	private static ArrayList<Point> ListPath(long fromNodeId, long toNodeID, List<CapybaraRouteNode> nodeList) {
-		System.out.println("From Node "+fromNodeId +" To Node "+ toNodeID);
-		
-		CapybaraRouteNode currentNode = nodeList.get((int) toNodeID);
-		List<CapybaraRouteNode> path = currentNode.getShortestPath();
-		
-		CapybaraRouteNode PreviousNode = null;
-		ArrayList<Point> listToSend = new ArrayList<>();
 
-		for(CapybaraRouteNode current : path) {
-			if(PreviousNode != null) {
-				List<Point> points = PreviousNode.getAdjacentNodes().entrySet().stream()
-						.filter(adjacencyPair -> adjacencyPair.getKey().getID() == current.getID())
-						.findFirst().get().getValue().coords;
-				listToSend.addAll(points);
-			}
-
-			PreviousNode = current;
-		}
-
-		List<Point> points = PreviousNode.getAdjacentNodes().entrySet().stream()
-				.filter(adjacencyPair -> adjacencyPair.getKey().getID() == currentNode.getID())
-				.findFirst().get().getValue().coords;
-		
-		listToSend.addAll(points);
-
-		return listToSend;
-	}
-	public static List<Point> GetRouteBetweenPoints(long startBuildingId, long endBuildingId, List<CapybaraRouteNode> nodeList){
-		long startIdx = startBuildingId - 1;
-		long endIdx = endBuildingId - 1;
-
-		CapybaraRouteGraph graph = new CapybaraRouteGraph();
-	    
-	    for(int i=0;i<nodeList.size();i++) {
-	    	graph.addNode(nodeList.get(i));
-	    }
-	    
-		System.out.println("Starting routing shortest from NodeID " + startIdx);
-	    
-		int found =-1;
-	    int found2 =-1;
-	    
-	    ArrayList<Point> toReturn = new ArrayList<>();
-	    
-	    for(int i=0;i<nodeList.size();i++) {
-	    	CapybaraRouteNode currentNode = nodeList.get(i);
-	    	int currentID = currentNode.getID();
-	    	if(startIdx == currentID) {
-	    		found = currentID;
-	    	}
-	    }
-	    
-	    if(found >= 0) {
-	    	CapybaraRouteNode startNode = nodeList.get(found);
-	    	CapybaraRouteGraph.calculateShortestPathFromSource(graph,startNode);
-		    	    	
-		    for(int i=0;i<nodeList.size();i++) {
-		    	CapybaraRouteNode currentNode = nodeList.get(i);
-		    	int currentID = currentNode.getID();
-		    	if(endIdx == currentID) {
-		    		found2 = currentID;
-		    	}
-		    }
-		    
-		    toReturn = ListPath(found,found2,nodeList);
-	    	
-		    System.out.println(toReturn.size());
-	    	
-	    }else {
-	    	
-	    	System.out.println("COULDN'T FIND ID");
-	    
-	    }
-	    return toReturn;
-	}
-	*/
 }
 
 
