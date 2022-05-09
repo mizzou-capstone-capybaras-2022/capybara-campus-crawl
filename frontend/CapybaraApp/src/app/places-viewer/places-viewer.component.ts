b9d70e7d80e9b837be75025cf1a3f2a17428b00c
Mechasparrow <mikipux7@gmail.com>
2022-05-09 10:01:52 -0500

    places picker created

--- frontend/CapybaraApp/src/app/places-viewer/places-viewer.component.ts
+++ frontend/CapybaraApp/src/app/places-viewer/places-viewer.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import {Place} from '../../services/crawl-api/model/place';
+
 
 @Component({
   selector: 'app-places-viewer',
   templateUrl: './places-viewer.component.html',
   styleUrls: ['./places-viewer.component.scss']
 })
-export class PlacesViewerComponent implements OnInit {
+export class PlacesViewerComponent {
 
-  constructor() { }
+  places:Array<Place.PlaceTypeEnum> = [];
 
-  ngOnInit(): void {
+  inputPlaceForm = new FormGroup({
+    placeType: new FormControl(Place.PlaceTypeEnum.Classroom)
+  });
+
+  constructor() {
+    this.places = Object.values(Place.PlaceTypeEnum);
+  }
+
+  getPlaceName(place: Place.PlaceTypeEnum) : string {
+    return place.toString().charAt(0).toUpperCase() + place.toString().slice(1).toLowerCase();
+  }
+
+  inputPlacesFn(): void {
+    
   }
 
 }
