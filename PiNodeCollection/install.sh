b2d61d0525c53875ccf5e5684e1f513cbc72da22
Isaac Milarsky <krabs@tilde.team>
2022-05-10 16:27:22 -0500

    Final touches after on-site on-metal test
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- PiNodeCollection/install.sh
+++ PiNodeCollection/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if [ "$EUID" -ne 0 ];
   then echo "Please run as root"
   exit 1
@@ -7,15 +8,17 @@ fi
 
 pip install -r ./requirements.txt
 #apt install aircrack-ng
-ip link set wlan1 down
-iw wlan1 set monitor none
-ip link set wlan1 up
+
+#Remember to change this to the desired interface name.
+ip link set wlp0s20f0u1 down
+iw wlp0s20f0u1 set monitor none
+ip link set wlp0s20f0u1 up
 
 cp ./MonitorNodes.py /root
 
 chmod +x /root/MonitorNodes.py
 
-cp ./env.json /etc/pi_env.json
+#cp ./env.json /etc/pi_env.json
 
 #Will run hourly
 (crontab -l 2>/dev/null; echo "0 * * * * python3 /root/MonitorNodes.py") | crontab -
